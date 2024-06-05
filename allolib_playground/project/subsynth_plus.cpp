547d7504093cc8940b4b3b7d5a9b939593496373
tianyusun7 <tianyusun@ucsb.edu>
2024-06-05 16:03:50 -0700

    update on final project demo

--- allolib_playground/project/subsynth_plus.cpp
+++ allolib_playground/project/subsynth_plus.cpp
@@ -26,7 +26,7 @@ public:
     gam::Pan<> mPan;
     gam::ADSR<> mAmpEnv;
     gam::EnvFollow<> mEnvFollow;  // envelope follower to connect audio output to graphics
-    gam::Sine<> mOsc; // gam::DSF<> mOsc;
+    gam::DSF<> mOsc; //  gam::Sine<> mOsc;
     gam::NoiseWhite<> mNoise;
     gam::Reson<> mRes;
     gam::Env<2> mCFEnv; //center freq envelope
@@ -43,8 +43,8 @@ public:
         mAmpEnv.sustainPoint(2); // Make point 2 sustain until a release is issued
         mCFEnv.curve(0);
         mBWEnv.curve(0);
-        mVibrato.freq(5); 
-        //mOsc.harmonics(12);
+        mVibrato.freq(10); 
+        mOsc.harmonics(12);
         // We have the mesh be a sphere
         addDisc(mMesh, 1.0, 30);
 
@@ -62,19 +62,21 @@ public:
         createInternalTriggerParameter("bw1", 700.0, 10.0, 5000);
         createInternalTriggerParameter("bw2", 900.0, 10.0, 5000);
         createInternalTriggerParameter("bwRise", 0.5, 0.1, 2);
-        createInternalTriggerParameter("vibratoRate", 0.1, 0.0, 12.0);
-        //createInternalTriggerParameter("hmnum", 12.0, 5.0, 20.0);
-        //createInternalTriggerParameter("hmamp", 1.0, 0.0, 1.0);
+        createInternalTriggerParameter("vibratoDepth", 0.1, 0.0, 5.0);
+        createInternalTriggerParameter("vibratoRate", 0.1, 0.0, 10.0);
+        createInternalTriggerParameter("hmnum", 12.0, 5.0, 20.0);
+        createInternalTriggerParameter("hmamp", 1.0, 0.0, 1.0);
         createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
-
     }
     
     virtual void onProcess(AudioIOData& io) override {
         updateFromParameters();
         float amp = getInternalParameterValue("amplitude");
         float noiseMix = getInternalParameterValue("noise");
+
         while(io()){
-            mOsc.freq(getInternalParameterValue("frequency") + mVibrato()); 
+            float vibratoDepth = getInternalParameter("vibratoDepth");
+            mOsc.freq(getInternalParameterValue("frequency") + vibratoDepth * mVibrato()); //larger bw, larger effect from vibrato
             // mix oscillator with noise
             float s1 = mOsc()*(1-noiseMix) + mNoise()*noiseMix;
 
@@ -92,7 +94,6 @@ public:
             io.out(1) += s2;
         }
         
-        
         if(mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
     }
 
@@ -102,7 +103,7 @@ public:
         g.pushMatrix();
 
         // Calculate vertical position based on frequency
-        float minFreq = 200.0f;  
+        float minFreq = 0.0f;  
         float maxFreq = 2000.0f;  
         float verticalPosition = ((frequency - minFreq) / (maxFreq - minFreq)) * 2.0f - 1.0f;
         //g.translate(amplitude, amplitude, -4);
@@ -110,8 +111,6 @@ public:
         //g.scale(frequency/2000, frequency/4000, 1);
         float scaling = 0.1;
         g.scale(scaling * frequency/200, scaling * frequency/400, scaling* 1);
-        //   cout << "mEnvFollow.value(): " << mEnvFollow.value() << endl;
-        //   cout << "freq/1000: " << frequency/1000 << endl;
         
         // Define a function to get a rainbow color based on frequency
         auto getRainbowColor = [](float freq) {
@@ -161,9 +160,8 @@ public:
 
     void updateFromParameters() {
         mOsc.freq(getInternalParameterValue("frequency"));
-        //mOsc.harmonics(getInternalParameterValue("hmnum"));
-        //mOsc.ampRatio(getInternalParameterValue("hmamp"));
-        mVibrato.freq(getInternalParameterValue("vibratoRate"));
+        mOsc.harmonics(getInternalParameterValue("hmnum"));
+        mOsc.ampRatio(getInternalParameterValue("hmamp"));
         mAmpEnv.attack(getInternalParameterValue("attackTime"));
     //    mAmpEnv.decay(getInternalParameterValue("attackTime"));
         mAmpEnv.release(getInternalParameterValue("releaseTime"));
@@ -176,7 +174,6 @@ public:
                       getInternalParameterValue("cf2"),
                       getInternalParameterValue("cf1"));
 
-
         mCFEnv.lengths()[0] = getInternalParameterValue("cfRise");
         mCFEnv.lengths()[1] = 1 - getInternalParameterValue("cfRise");
         mBWEnv.levels(getInternalParameterValue("bw1"),
@@ -187,6 +184,7 @@ public:
 
         mCFEnv.totalLength(getInternalParameterValue("envDur"));
         mBWEnv.totalLength(getInternalParameterValue("envDur"));
+        mVibrato.freq(getInternalParameter("vibratoRate")); 
     }
 };
 
